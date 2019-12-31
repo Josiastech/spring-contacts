0110059c11049e2e1b70f18e84619ce62519e620
Josias <josiastech@gmail.com>
2019-12-31 16:57:30 -0600

    Add basic CRUD functionality

--- src/main/java/com/josias/contacts/ContactsApplication.java
+++ src/main/java/com/josias/contacts/ContactsApplication.java
@@ -1,7 +1,13 @@
 package com.josias.contacts;
 
+import com.josias.contacts.model.Contact;
+import com.josias.contacts.repository.ContactRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.LongStream;
 
 @SpringBootApplication
 public class ContactsApplication {
@@ -10,4 +16,20 @@ public class ContactsApplication {
 		SpringApplication.run(ContactsApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(ContactRepository repository) {
+		return args -> {
+			repository.deleteAll();
+			LongStream.range(1, 11)
+					.mapToObj(i -> {
+						Contact c = new Contact();
+						c.setName("Contact " + i);
+						c.setEmail("contact" + i + "@email.com");
+						c.setPhone("(111) 111-1111");
+						return c;
+					})
+					.map(v -> repository.save(v))
+					.forEach(System.out::println);
+		};
+	}
 }
